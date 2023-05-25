2eb05f1c72280a8f29caff1bf2410ab4205bab46
reinerkp <kate.reiner1@gmail.com>
2023-05-24 20:29:05 -0600

    workbox

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,3 +1,5 @@
+
+const { StaleWhileRevalidate } = require('workbox-strategies');
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
